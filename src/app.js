03cb88e822a016e3b00a2bb18cb9142ce3a326e3
TCFII <168501077+TCFII@users.noreply.github.com>
2024-06-03 23:31:24 +0000

    first_code

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const pronouns = ["the", "our"];
+const adjs = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const exts = [".com", ".net", ".us"];
+const domains = [];
+
+for (const p in pronouns) {
+  for (const a in adjs) {
+    for (const n in nouns) {
+      for (const e in exts) {
+        const d = p + a + n + e;
+        domains.push(d);
+      }
+    }
+  }
+}
